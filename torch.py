bc8eb5845b70256289668a78c79cf3acc898529b
royw99 <115151868+royw99@users.noreply.github.com>
2023-08-08 08:21:44 +0800

    Update torch.py

--- torch.py
+++ torch.py
@@ -2014,38 +2014,25 @@ def copyfile(filename, target_dir):
     os.makedirs(target_dir, exist_ok=True)
     shutil.copy(filename, target_dir)
 
-def reorg_train_valid(data_dir, labels, valid_ratio):
-    """Split the validation set out of the original training set.
-
-    Defined in :numref:`sec_kaggle_cifar10`"""
-    # The number of examples of the class that has the fewest examples in the
-    # training dataset
+def reorg_train_valid(data_dir, labels, valid_ratio, work_dir):
     n = collections.Counter(labels.values()).most_common()[-1][1]
-    # The number of examples per class for the validation set
-    n_valid_per_label = max(1, math.floor(n * valid_ratio))
+    n_valid_per_label = max(1,math.floor(n * valid_ratio))
     label_count = {}
-    for train_file in os.listdir(os.path.join(data_dir, 'train')):
+    for train_file in os.listdir(os.path.join(data_dir,'train')):
         label = labels[train_file.split('.')[0]]
-        fname = os.path.join(data_dir, 'train', train_file)
-        copyfile(fname, os.path.join(data_dir, 'train_valid_test',
-                                     'train_valid', label))
-        if label not in label_count or label_count[label] < n_valid_per_label:
-            copyfile(fname, os.path.join(data_dir, 'train_valid_test',
-                                         'valid', label))
-            label_count[label] = label_count.get(label, 0) + 1
+        fname = os.path.join(data_dir,'train',train_file)
+        copyfile(fname,os.path.join(work_dir,'train_valid_test','train_valid',label)) 
+        if label not in label_count or label_count[label] < n_valid_per_label:                           
+            copyfile(fname,os.path.join(work_dir,'train_valid_test','valid',label))
+            label_count[label] = label_count.get(label,0) + 1
         else:
-            copyfile(fname, os.path.join(data_dir, 'train_valid_test',
-                                         'train', label))
+            copyfile(fname,os.path.join(work_dir,'train_valid_test','train',label))  
     return n_valid_per_label
 
-def reorg_test(data_dir):
-    """Organize the testing set for data loading during prediction.
-
-    Defined in :numref:`sec_kaggle_cifar10`"""
-    for test_file in os.listdir(os.path.join(data_dir, 'test')):
-        copyfile(os.path.join(data_dir, 'test', test_file),
-                 os.path.join(data_dir, 'train_valid_test', 'test',
-                              'unknown'))
+def reorg_test(data_dir,work_dir):
+    for test_file in os.listdir(os.path.join(data_dir,'test')):
+        copyfile(os.path.join(data_dir,'test',test_file),
+                os.path.join(work_dir,'train_valid_test','test','unknown')) # unknown为 test文件夹里面的一个文件夹   
 
 d2l.DATA_HUB['dog_tiny'] = (d2l.DATA_URL + 'kaggle_dog_tiny.zip',
                             '0cb91d09b814ecdc07b50f31f8dcad3e81d6a86d')
